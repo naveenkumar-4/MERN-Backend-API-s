1f21851346439de2dde201e922888a7cf38175dd
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-07-04 15:20:25 +0530

    update fileupload with multer

--- Social-Media-API-with-Mongoose/middlewares/uploadMiddleware.js
+++ Social-Media-API-with-Mongoose/middlewares/uploadMiddleware.js
@@ -1,22 +1,37 @@
-import multer from 'multer';
+// import multer from 'multer';
 
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "./uploads");
-    },
-    filename: (req, file, cb) => {
-      // cb(null, `${Date.now()}-${file.originalname}`);
-      cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, "./uploads");
+//     },
+//     filename: (req, file, cb) => {
+//       // cb(null, `${Date.now()}-${file.originalname}`);
+//       cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
   
-    },
-  });
+//     },
+//   });
+
+// const upload = multer({
+//   limits: {
+//     fileSize: 1000000
+//   },
+//   storage:storage,
+//   fileFilter(req, file, cb) {
+//     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+//       return cb(new Error('Please upload an image'));
+//     }
+//     cb(null, true);
+//   }
+// });
+
+// export default upload;
+import multer from 'multer';
 
 const upload = multer({
   limits: {
     fileSize: 1000000
   },
-  storage:storage,
   fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
       return cb(new Error('Please upload an image'));
